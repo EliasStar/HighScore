221be56878a08d38515f5d751fd414e38f6f535a
Elias* <elias.s.reiner@gmail.com>
2020-04-07 20:06:49 +0200

    Docker setup complete!

--- wait-for-exit.sh
+++ wait-for-exit.sh
@@ -1,6 +1,10 @@
-while ping -c 1 $1 &> /dev/null do
-sleep 1;
-done;
+#!/bin/sh
+
+while ping -c 1 $1 >/dev/null 2>&1
+    do sleep 1
+done
 
 echo "$1 finished!"
+
+export PATH="$PATH:$SCRIPT_PATH"
 exec $2
