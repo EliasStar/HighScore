457f182461df532db0963afac3b91c8fee7f3825
Elias Reiner <elias.s.reiner@gmail.com>
2020-04-12 22:55:39 +0200

    student module

--- models/student.ts
+++ models/student.ts
@@ -1,13 +1,34 @@
 import http2 from 'http2';
 
 const client = http2.connect('https://www.dachsberg.at:443', () => console.log('[Database] Connected to Dachsberg.'));
-let studentList: [{}];
+let studentList: Student[];
+let classes: { [id: string]: ClassName };
+
+type ClassName = string;
+
+export type Student = {
+    id: string,
+    name: {
+        first: string
+        last: string
+    },
+    class: ClassName,
+    gender: 'male' | 'female'
+}
+
+interface InternalStudent {
+    studentId: string,
+    firstName: string,
+    lastName: string,
+    cls: string,
+    gender: 'M' | 'W'
+}
 
 client.on('error', err => console.error('[Database] Error from client: ' + err));
 
 export function updateStudentList() {
     const body = 'request=studentList';
-    let data: string;
+    let data = '';
     let status: number | undefined;
 
     client.request({
@@ -19,7 +40,20 @@ export function updateStudentList() {
         if (typeof status === 'number') {
             switch (status) {
                 case 200:
-                    studentList = JSON.parse(data);
+                    let students: InternalStudent[] = JSON.parse(data);
+                    studentList = students.map(student => {
+                        return {
+                            id: student.studentId,
+                            name: {
+                                first: student.firstName,
+                                last: student.lastName
+                            },
+                            class: student.cls,
+                            gender: student.gender === 'M' ? 'male' : 'female'
+                        }
+                    });
+                    //Sort
+                    //Fill classnames
                     console.log('[Database] Updated student list.');
                     break;
 
@@ -34,6 +68,33 @@ export function updateStudentList() {
             console.error('[Database] Could not get status while updating student list.');
         }
     }).on('error', err => console.error('[Database] Error while updating student list: ' + err)).end(body);
+}
+
+export function nameForID(id: string): string {
+    studentList.forEach(student => {
+        if (student.id === id) {
+            return `${student.name.first} ${student.name.last}`;
+        }
+    });
+    return '';
+}
+
+export function findAll(gender: 'male' | 'female' | 'both', className: 'all' | ClassName): Student[] {
+    let result: Student[] = [];
+    studentList.forEach(student => {
+        if ((className === 'all' || student.class === className) && (gender === 'both' || student.gender === gender)) {
+            result.push(student);
+        }
+    });
+    return result;
+}
+
+export function toClass(className: string): ClassName | undefined {
+    return classes[className.toUpperCase()] ? classes[className.toUpperCase()] : undefined;
+}
+
+export function getClasses(): ClassName[] {
+    return [];
 }
 
 export function closeClient() {
